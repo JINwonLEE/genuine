655e7705c56f44c2f02ff556e0628db83ff70422
Jinwon Lee <dlwlsdnjsehs1993@gmail.com>
2019-02-04 17:29:51 +0900

    Prac

--- BaekJoon/sorting/10989.cpp
+++ BaekJoon/sorting/10989.cpp
@@ -1,42 +1,13 @@
 #include <iostream>
 
-//void countingSort(int* arr, int size);
 
 using namespace std;
-
-/*
-void countingSort(int* arr, int size) {
-    int* counting = new int[10001];
-    int* tmp = new int[size];
-    fill_n(counting, 10001, 0);
-    
-    for (int i = 0; i < size; i++) {
-        counting[arr[i]]++;
-    }
-    
-    for (int i = 0; i < 10001; i++) {
-        counting[i] += counting[i -1];
-    }
-    
-    for (int i = 0; i < size; i++) {
-        tmp[counting[arr[i]] - 1] = arr[i];
-        counting[arr[i]]--;
-    }
-    
-    for (int i = 0; i < size; i++) {
-        arr[i] = tmp[i];
-    }   
-    
-    delete [] counting;
-    delete [] tmp;
-}
-*/
 
 int main() {
     int num;
     cin >> num;
-    int count[10001];
-    fill_n(count, 10001, 0);
+    int count[10000001];
+    fill_n(count, 10000001, 0);
     
     int val;
     for (int i = 0; i < num; i++) {
