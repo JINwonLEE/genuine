85ad9978a5783e8b96a2b778a14a32aca9f971a1
Jinwon Lee <dlwlsdnjsehs1993@gmail.com>
2019-10-02 23:33:56 +0900

    [Problem Solved] Baekjoon Problem 2504

--- BaekJoon/stack/2504.cpp
+++ BaekJoon/stack/2504.cpp
@@ -1,109 +1,96 @@
 #include <iostream>
 #include <stack>
+#include <stdexcept>
 #include <string>
 
 using namespace std;
 
 int main() {
-    string p;
-    cin >>p;
-    int sum = 0;
-    stack<int> st;
-    bool wrong = false; // '(' = -1, ')' = -2, '[' = -3, ']' = -4
+    stack<string> stack_input;
 
-    for (int i = 0; i < p.length(); i++) {
-        if (p[i] == '(') {
-            st.push(-1);
-        }
-        else if (p[i] == ')') {
-            if (st.top() != -1 && st.top() < 0) {
-                wrong = true;
-                sum = 0;
+    int result = 0;
+    string input;
+    cin >> input;
+    bool wrong = false;
+
+    for (auto& i : input) {
+        if (i == '(' || i == '[') {
+            stack_input.push(string(1, i));
+        } else if (i == ')') {
+            if (stack_input.empty()) {
+                result = 0;
                 break;
             }
-            else {
-                if (st.top() == -1) {
-                    st.pop();
-                    st.push(2);
-                }
-                else {
-                    int val = 0;
-                    while (st.top() != -1) {
-                        if (st.top() > 0) {
-                            val += st.top();
-                            st.pop();
-                        }
-                        else {
-                            wrong = true;
-                            sum = 0;
-                            break;
-                        }
-                    }
-                    if (wrong) break;
-                    val *= 2;
-                    st.pop();
-                    if (st.empty()) {
-                        sum += val;
-                    }
-                    else {
-                        st.push(val);
+            string top = stack_input.top();
+            if (top == "(") {
+                stack_input.pop();
+                stack_input.push("2");
+            } else {
+                int num = 0;
+                while (!stack_input.empty() && stack_input.top() != "(") {
+                    try {
+                        num += stoi(stack_input.top());
+                        stack_input.pop();
+                    } catch (...) {
+                        wrong = true;
+                        num = 0;
+                        break;
                     }
                 }
+
+                if (wrong || stack_input.empty()) {
+                    result = 0;
+                    break;
+                }
+
+                stack_input.pop();
+                num *= 2;
+                stack_input.push(to_string(num));
             }
-        }
-        else if (p[i] == '[') {
-            st.push(-3);
-        }
-        else if (p[i] == ']') {
-            if (st.top() != -3 && st.top() < 0) {
-                wrong = true;
-                sum = 0;
+        } else if (i == ']') {
+            if (stack_input.empty()) {
+                result = 0;
                 break;
             }
-            else {
-                if (st.top() == -3) {
-                    st.pop();
-                    st.push(3);
-                }
-                else {
-                    int val = 0;
-                    while (st.top() != -3) {
-                        if (st.top() > 0) {
-                            val += st.top();
-                            st.pop();
-                        }
-                        else {
-                            wrong = true;
-                            sum = 0;
-                            break;
-                        }
-                    }
-                    if (wrong) break;
-                    val *= 3;
-                    st.pop();
-                    if (st.empty()) {
-                        sum += val;
-                    }
-                    else {
-                        st.push(val);
+            string top = stack_input.top();
+            if (top == "[") {
+                stack_input.pop();
+                stack_input.push("3");
+            } else {
+                int num = 0;
+                while (!stack_input.empty() && stack_input.top() != "[") {
+                    try {
+                        num += stoi(stack_input.top());
+                        stack_input.pop();
+                    } catch (...) {
+                        wrong = true;
+                        num = 0;
+                        break;
                     }
                 }
+
+                if (wrong || stack_input.empty()) {
+                    result = 0;
+                    break;
+                }
+
+                stack_input.pop();
+                num *= 3;
+                stack_input.push(to_string(num));
             }
         }
     }
-    while (!st.empty()) {
-        if (st.top() > 0) {
-            int val = st.top();
-            st.pop();
-            sum += val;
-        }
-        else {
-            wrong = true;
-            sum = 0;
+
+    while (!stack_input.empty()) {
+        string num = stack_input.top();
+        try {
+            result += stoi(num);
+        } catch (...) {
+            result = 0;
             break;
         }
+        stack_input.pop();
     }
-
-    cout << sum << "\n";
+    cout << result << endl;
     return 0;
 }
