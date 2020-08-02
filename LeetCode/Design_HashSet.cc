10c9e0268add42c95bd0f1a09144ac548490e162
JINwonLEE <dlwlsdnjsehs1993@gmail.com>
2020-08-02 18:51:22 +0900

    [Problem Solved] Leet Code Problem Design HashSet

---
+++ LeetCode/Design_HashSet.cc
@@ -0,0 +1,28 @@
+class MyHashSet {
+public:
+    set<int> container;
+    /** Initialize your data structure here. */
+    MyHashSet() {
+    }
+    
+    void add(int key) {
+        container.insert(key);
+    }
+    
+    void remove(int key) {
+        if (container.count(key)) container.erase(key);
+    }
+    
+    /** Returns true if this set contains the specified element */
+    bool contains(int key) {
+        return container.count(key);
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
