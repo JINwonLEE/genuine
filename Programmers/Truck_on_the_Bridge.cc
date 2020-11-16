b56c32805e3b2cc75e87167c2a51ed095b8b5299
JINwonLEE <dlwlsdnjsehs1993@gmail.com>
2020-11-16 22:37:46 +0900

    [Problem Solved] Programmers Problem Truck on the Bridge

---
+++ Programmers/Truck_on_the_Bridge.cc
@@ -0,0 +1,55 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+struct item {
+    int t;
+    int remain;
+};
+
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+    int answer = 0;
+    queue<int> trucks;
+    //sort(truck_weights.begin(), truck_weights.end());
+    for (auto t : truck_weights) {
+        trucks.push(t);
+    }
+    
+    vector<item> on_bridge;
+    int remain = weight;
+    int count = 0;
+    while (!trucks.empty()) {
+        count++;
+        for (int i = 0; i < on_bridge.size(); i++) {
+            if (on_bridge[i].remain == 0) {
+                remain += on_bridge[i].t;
+            }
+        }
+        int cur = trucks.front();
+        if (remain >= cur) {
+            remain -= cur;
+            trucks.pop();
+            on_bridge.push_back({cur, bridge_length});
+        }
+
+        for (int i = 0; i < on_bridge.size(); i++) {
+            on_bridge[i].remain--;
+        }
+    }
+    
+    while (true) {
+        bool isDone = true;
+        for (int i = 0; i < on_bridge.size(); i++) {
+            if (on_bridge[i].remain >= 0) {
+                isDone = false;
+                on_bridge[i].remain--;
+            }
+        }
+        if (isDone) break;
+        count++;
+    }
+    
+    return count;
+}
